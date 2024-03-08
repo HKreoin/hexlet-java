bb040fe656358b3562b1750d085ad944c47b69e4
HKreoin <steamuser85@yandex.ru>
2024-03-08 19:48:30 +0300

    mv One and Two classes

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,4 +1,4 @@
-package io;
+package io.hexlet;
 
 public class One {
     public static void main(String[] args) {
--- io/hexlet/Two.java
+++ io/hexlet/Two.java
@@ -1,4 +1,4 @@
-package io;
+package io.hexlet;
 
 public class Two {
     public static void greeting() {
